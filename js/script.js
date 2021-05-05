3c156a495d940fc0917308effd2fe7a795728d6f
bosciu <bosciu@icloud.com>
2021-05-05 16:24:46 +0200

    Completamento esercizo con parte bonus

--- js/script.js
+++ js/script.js
@@ -40,28 +40,38 @@ function isInArray(element, array) {
     }
 }
 /* /FUNCTIONS */
-
-
-var maxAttempts = 84;
-/* Linea debug */
-maxAttempts = 5;
+var maxNumber = 100;
+var maxAttempts = maxNumber - 16;
 var bombs = [];
 var userChoice = [];
 var gameOver = false;
+var difficulty = parseInt(prompt("Scegli la difficoltà tra: facile (1), medio (2), difficile (3)"));
+switch (difficulty) {
+    case 1:
+        maxNumber = 100;
+        break;
+    case 2:
+        maxNumber = 80;
+        break;
+    case 3:
+        maxNumber = 50;
+        break;
+    default:
+        alert("Errore nella selezione della difficoltà. Verrà impostato facile di default.");
+}
 
 /* Generare 16 numeri casuali e verificare non ci siano duplicati */
 while (bombs.length < 16) {
-    var numeroCasuale = getRandomNumber(1, 100);
+    var numeroCasuale = getRandomNumber(1, maxNumber);
     if (!isInArray(numeroCasuale, bombs)) {
         bombs.push(numeroCasuale);
     }
 }
-console.log(bombs);
 
 /* Chiedi all'utente 100 - 16 volte di inserire un numero, niente doppioni */
 while (userChoice.length < maxAttempts && gameOver == false) {
-    var numeroUtente = parseInt(prompt("Inserisci un numero tra 1 e 100!"));
-    if (!isInArray(numeroUtente, userChoice) && numeroUtente > 0 && numeroUtente < 101 && !isNaN(numeroUtente)) {
+    var numeroUtente = parseInt(prompt("Inserisci un numero tra 1 e " + maxNumber + "!"));
+    if (!isInArray(numeroUtente, userChoice) && numeroUtente > 0 && numeroUtente < maxNumber + 1 && !isNaN(numeroUtente)) {
         if (isInArray(numeroUtente, bombs)) {
             gameOver = true;
             alert("GAME OVER\nHai colpito la bomba!\nHai totalizzato: " + userChoice.length + " punti.");
@@ -73,4 +83,5 @@ while (userChoice.length < maxAttempts && gameOver == false) {
 if (!gameOver) {
     alert("Complimenti! Hai vinto!\nHai totalizzato: " + userChoice.length + " punti.")
 }
+console.log("BOMBE", bombs, "SCELTA UTENTE", userChoice);
 /* DEBUG */
